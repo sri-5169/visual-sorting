8c8688d1337add8ff18ef61eba0638fdbd2eab88
Srinath Pandey <72240749+sri-5169@users.noreply.github.com>
2022-06-02 09:59:29 +0530

    coded the bubble sort and its optimised version

---
+++ scripts/animations.js
@@ -0,0 +1,41 @@
+var speed = 1000;
+input_algo_speed.addEventListener("input", visualization_speed);
+function visualization_speed() {
+  var algo_speed = input_algo_speed.value;
+  switch (parseInt(algo_speed)) {
+    case 1:
+      speed = 1;
+      break;
+    case 2:
+      speed = 10;
+      break;
+    case 3:
+      speed = 100;
+      break;
+    case 4:
+      speed = 1000;
+      break;
+    case 5:
+      speed = 10000;
+      break;
+  }
+  delay_time = 10000 / (Math.floor(array_size / 10) * speed);
+}
+
+var delay_time = 10000 / (Math.floor(array_size / 10) * speed);
+var c_delay = 0;
+
+function div_update(cont, height, color) {
+  window.setTimeout(function () {
+    cont.style =
+      " margin:0% " +
+      margin_size +
+      "%; width:" +
+      (100 / array_size - 2 * margin_size) +
+      "%; height:" +
+      height +
+      "%; background-color:" +
+      color +
+      ";";
+  }, (c_delay += delay_time));
+}
---
+++ scripts/optimised_bubble_sort.js
@@ -0,0 +1,34 @@
+function Optimised_Bubble_sort() {
+  document.getElementById("Worst_Time").innerText = "O(N^2)";
+  document.getElementById("Average_Time").innerText = "Θ(N^2)";
+  document.getElementById("Best_Time").innerText = "Ω(N)";
+  document.getElementById("Worst_Space").innerText = "O(1)";
+
+  c_delay = 0;
+  for (var i = 0; i < array_size; i++) {
+    var flag = false;
+    for (var j = 0; j < array_size - i - 1; j++) {
+      div_update(divs[j], div_sizes[j], "yellow"); // Color Update
+
+      if (div_sizes[j] > div_sizes[j + 1]) {
+        // change color of both
+        flag = true;
+        div_update(divs[j], div_sizes[j], "red");
+        div_update(divs[j + 1], div_sizes[j + 1], "red");
+        var temp = div_sizes[j];
+        div_sizes[j] = div_sizes[j + 1];
+        div_sizes[j + 1] = temp;
+        div_update(divs[j], div_sizes[j], "red");
+        div_update(divs[j + 1], div_sizes[j + 1], "red");
+      }
+      // make the current element blue it has been compared
+      div_update(divs[j], div_sizes[j], "blue");
+    }
+    // make the lasst one green ie reached its correct position
+    div_update(divs[j], div_sizes[j], "green");
+    if (flag == false) break;
+  }
+  div_update(divs[0], div_sizes[0], "green");
+  // enable the disabled buttons
+  // enable_buttons();
+}
